93702844b89b1eca7ddc2a22faa632f822a86dba
manasabharath <86051093+manasabharath@users.noreply.github.com>
2021-07-07 01:20:20 +0530

    Add files via upload

---
+++ BmiCalculatorLibraryApp/BmiVaildationLib/BmiValidationType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmiVaildationLib
+{
+    public class BmiValidationType
+    {
+        public string CheckCondition(int BMI)
+        {
+            if (BMI < 18.5)
+            {
+                return "UnderWeight";
+            }
+            else if (BMI >= 25)
+            {
+                return "OverWeight";
+            }
+            else
+            {
+                return "Normal";
+            }
+
+        }
+    }
+}
