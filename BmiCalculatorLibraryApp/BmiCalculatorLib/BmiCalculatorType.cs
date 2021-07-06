4930b85d118f13e56f62af956e32dc5a89269da8
manasabharath <86051093+manasabharath@users.noreply.github.com>
2021-07-07 01:04:50 +0530

    Add files via upload

---
+++ BmiCalculatorLibraryApp/BmiCalculatorLib/BmiCalculatorType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmiCalculatorLib
+{
+    public class BmiCalculatorType
+    {
+        private int BMI;
+        public int calculator(int height, int weight)
+        {
+            BMI = weight / height;
+            return BMI;
+        }
+    }
+}
