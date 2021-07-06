915cc120c0ee43d30e528078417c92501ea7a6e5
manasabharath <86051093+manasabharath@users.noreply.github.com>
2021-07-07 01:20:48 +0530

    Add files via upload

---
+++ BmiCalculatorLibraryApp/BmiDisplayLib/BmiDisplayType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmiDisplayLib
+{
+    public class BmiDisplayType
+    {
+        public void Display(string message)
+        {
+
+            Console.WriteLine(message);
+        }
+    }
+}
