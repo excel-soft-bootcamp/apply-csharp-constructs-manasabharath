c13e6c0b2aaca0c2ddb62a6f479ee1044c7d8ee2
manasabharath <86051093+manasabharath@users.noreply.github.com>
2021-07-07 01:32:36 +0530

    Add files via upload

---
+++ BmiCalculatorLibraryApp/UserInputLib/UserInputType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInputLib
+{
+    public class UserInputType
+    {
+        public int Height()
+        {
+            int height = int.Parse(this.ReadFromConsole("Enter your height:"));
+            return height;
+        }
+
+        public int Weight()
+        {
+            int weight = int.Parse(this.ReadFromConsole("Enter your weight:"));
+            return weight;
+        }
+        private string ReadFromConsole(string message)
+        {
+
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            return input;
+        }
+    }
+}
