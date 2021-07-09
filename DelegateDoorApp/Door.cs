3ef25f8f976036959867f1f7683a33ce8a6906d6
manasabharath <86051093+manasabharath@users.noreply.github.com>
2021-07-09 19:06:50 +0530

    Add files via upload

---
+++ DelegateDoorApp/Door.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateChildApp
+{
+
+    public delegate void DoorStatusState(string status);
+    //Observable
+    public  class Door
+    {
+        string DoorStatus;
+       public event DoorStatusState DoorStatusStateChanged;
+        public void Open()
+        {
+            DoorStatus = "open";
+            DoorStatusStateChanged.Invoke(this.DoorStatus);
+
+
+
+}
+        public void Close()
+        {
+             DoorStatus = "close";
+
+            DoorStatusStateChanged.Invoke(this.DoorStatus);
+        }
+
+        
+    }
+    
+}
+    
+
---
+++ DelegateDoorApp/SecuritySystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateChildApp
+{
+   public  class SecuritySystem
+    {
+        public void Notify(string status)
+        {
+            this.SMS();
+        }
+        public void SMS()
+        {
+            
+        }
+    }
+}
